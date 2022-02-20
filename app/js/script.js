a5f82bac941de2c2c1b06fc113599eae5cd207ae
ar78ilnik <arakchiev.ilya@yandex.ru>
2022-02-20 22:17:13 +0300

    Img

--- app/js/script.js
+++ app/js/script.js
@@ -1,24 +1,12 @@
 let header = document.querySelector('.header'),
-	headerMenu = header.querySelector('.header__menu'),
-	headerPopup = header.querySelector('.header__popup'),
-	bodyBack = document.querySelector('.body__back'),
+	button = header.querySelector('.button_nav'),
 	listMenu = header.querySelector('.list_menu');
 
-headerMenu.addEventListener('click', () => {
-	let expanded = headerMenu.getAttribute('aria-expanded') === 'true' || false;
-    headerMenu.setAttribute('aria-expanded', !expanded);
-	headerPopup.classList.toggle('header__popup_open');
-	bodyBack.classList.toggle('body__back_opened')
-	document.body.style.overflow = 'hidden';
+button.addEventListener('click', () => {
+	let expanded = button.getAttribute('aria-expanded') === 'true' || false;
+    button.setAttribute('aria-expanded', !expanded);
+    button.classList.toggle('button_nav-open');
+    listMenu.classList.toggle('list_menu-open');
+    header.classList.toggle('header_open');
 });
 
-    bodyBack.addEventListener('click', (event) => {
-        if (event.target === bodyBack) {
-            console.log('1');
-            let expanded = headerMenu.getAttribute('aria-expanded') === 'true' || false;
-            headerMenu.setAttribute('aria-expanded', !expanded);
-            headerPopup.classList.toggle('header__popup_open');
-            bodyBack.classList.toggle('body__back_opened');
-            document.body.style.overflow = '';
-        }
-    })
